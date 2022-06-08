48cd9f8ce5fa9932922074a8bb83960c3865f785
John Tuyen <john.tuyen@gmail.com>
2022-06-07 22:40:41 -0400

    Create shiftit_init.lua

---
+++ hammerspoon/shiftit_init.lua
@@ -0,0 +1,15 @@
+hs.loadSpoon("ShiftIt")
+spoon.ShiftIt:bindHotkeys({
+    left = {{ 'shift', 'ctrl', 'alt', 'cmd' }, 'left' },
+    right = {{ 'shift', 'ctrl', 'alt', 'cmd' }, 'right' },
+    up = {{ 'shift', 'ctrl', 'alt', 'cmd' }, 'up' },
+    down = {{ 'shift', 'ctrl', 'alt', 'cmd' }, 'down' },
+    upleft = {{ 'shift', 'ctrl', 'alt', 'cmd' }, '1' },
+    upright = {{ 'shift', 'ctrl', 'alt', 'cmd' }, '2' },
+    botleft = {{ 'shift', 'ctrl', 'alt', 'cmd' }, '3' },
+    botright = {{ 'shift', 'ctrl', 'alt', 'cmd' }, '4' },
+    maximum = {{ 'shift', 'ctrl', 'alt', 'cmd' }, 'm' },
+    center = {{ 'shift', 'ctrl', 'alt', 'cmd' }, 'v' },
+    nextScreen = {{ 'shift', 'ctrl', 'alt', 'cmd' }, 't' },
+    previousScreen = {{ 'shift', 'ctrl', 'alt', 'cmd' }, 'r' },
+})
